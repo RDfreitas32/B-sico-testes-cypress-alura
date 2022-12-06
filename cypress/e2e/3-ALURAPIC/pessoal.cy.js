d196bba81dcd3004a05c89269a25d4524ece6b05
RodrigoF32 <rodrigofreitas3223@outlook.com>
2022-12-06 20:25:25 -0300

    Testando outros sites após erros

--- cypress/e2e/3-ALURAPIC/pessoal.cy.js
+++ cypress/e2e/3-ALURAPIC/pessoal.cy.js
@@ -5,10 +5,10 @@ describe('Login site alura', () => {
 
     })
     it('fazendo login na UAM', () => {
-        cy.login_uam('41784248827', 'Dinekima32');
-        cy.get('button[class="button mb-5 is-primary is-rounded"]').should('be.visible');
-        cy.get('button[class="button mb-5 is-primary is-rounded"]').click();
+        cy.login_uam('xxx', 'xxx');
         cy.get('h2[class="title is-1 mx-5 mt-1"]').should('be.visible');
+        cy.get('a[class="column is-one-fifth-desktop is-4-tablet is-5-mobile app-card ulife"]').click();
+        cy.contains('body', 'class="ng-scope vsc-initialized"').should('be.visible');
     })
 
 })
